72c2bca08ea48576383657b17c176b6d6c7a19cb
Geoff Lawler <glawler@tislabs.com>
2017-03-18 14:24:21 -0700

    tcpdump agent now works. slightly updated interface is described in teh IDL. Supports coping cpautre files to central location now as well.

--- tcpdump/tcpdump.py
+++ tcpdump/tcpdump.py
@@ -1,69 +1,113 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2012 University of Southern California
+# Copyright (C) 2012, 2017 University of Southern California
 # This software is licensed under the GPLv3 license, included in
 # ./GPLv3-LICENSE.txt in the source distribution
 
 from magi.testbed import testbed
 from magi.util.agent import agentmethod, DispatchAgent
 from magi.util.processAgent import initializeProcessAgent
-from subprocess import Popen, PIPE
-import logging
-import os
-import signal
-import sys
-import time
+from subprocess import Popen
+from subprocess import STDOUT
 
+from os.path import exists, isdir, basename, join as path_join
+from os import makedirs
+from time import sleep
+from shutil import copyfile
+import logging
 
 log = logging.getLogger(__name__)
+
+class TcpdumpAgentException(Exception):
+    pass
 
 class TcpdumpAgent(DispatchAgent):
     '''
     '''
     def __init__(self):
         DispatchAgent.__init__(self)
-        self.dumpfile = "/tmp/tcpdump.cap"
-        self.destination = None
-        self.pid = None
-        
+        self.dumpfile = path_join('/', 'tmp', 'tcpdump.cap')
+        self.agentlog = path_join('/', 'tmp', 'tcpdump_agent.log')
+
+        # "private"
+        self._proc = None
+        self._lfile = None
+
     @agentmethod()
-    def startCollection(self, msg):
+    def startCollection(self, msg, expression, dumpfile=None, tcpdump_args=''):
+        if self._proc:
+            log.info('tcpdump already running. Stopping it so we can restart it...')
+            self.stopCollection(None)
+
         log.info("starting collection")
-        if not self.destination:
-            raise AttributeError("Destination not set")
-        log.info("destination: %s" %(self.destination))
-        interface = self.node2Intf(self.destination)
-        log.info("interface: %s" %(interface))
-        tcpdumpCmd = ['tcpdump', '-i', interface, '-w', self.dumpfile]
-        log.info('running: %s', ' '.join(tcpdumpCmd))
-        p = Popen(tcpdumpCmd, stdout=PIPE, stderr=PIPE)
-        time.sleep(1)
-        if p.poll():
+        # GTL - if needed add route table query to get correct iface "pointing" to a given host
+        # and replace "expression with "host destination" or something.
+        cmd = 'tcpdump -i any'
+        df = dumpfile if dumpfile else self.dumpfile
+        cmd += ' -w {}'.format(df)
+        if tcpdump_args:
+            cmd += ' {}'.format(tcpdump_args)
+
+        cmd += ' {}'.format(expression)
+
+        log.info('running: {}'.format(cmd))
+
+        # Do not remove the stdout, stderr redirection! It turns out tcpdump really doesn't
+        # like not having a stdout/err and will die if this is removed.
+        self._lfile = open(self.agentlog, 'w')
+        self._proc = Popen(cmd.split(), close_fds=True, stdout=self._lfile, stderr=STDOUT)
+
+        sleep(1)
+        if not self._proc or self._proc.poll():
             log.info('Could not start tcpdump')
+            self._proc = None
             return False
-        self.pid = p.pid
-        log.info('tcpdump started with process id %d' %(self.pid))
+
+        log.info('tcpdump started with process id {}'.format(self._proc.pid))
         return True
     
     @agentmethod()
     def stopCollection(self, msg):
         log.info("stopping collection")
-        log.info("killing tcpdump process %d", self.pid)
-        if self.pid:
-            os.kill(self.pid, signal.SIGTERM)
+        if not self._proc:
+            log.warn('No process running. Igorning stop.')
+            return True
+
+        if self._proc:
+            try:
+                self._proc.terminate()
+                sleep(1)
+                self._proc.kill()
+                self._proc.wait()
+            except OSError as e:
+                log.error('error stopping tcpdump process: {}'.format(e))
+
+            self._lfile.close()
+            self._proc = None
+
         return True
-    
-    def node2Intf(self, destination):
-        topoGraph = testbed.getTopoGraph()
-        src = testbed.getNodeName()
-        linkname = topoGraph[src][destination]['linkName']
-        srcLinks = topoGraph.node[src]['links']
-        try:
-            ip = srcLinks[linkname]['ip']
-            return testbed.getInterfaceInfo(ip).name
-        except Exception:
-            raise Exception("Invalid information. Mostly no direct link to destination '%s'." %(destination))
-                
+  
+    @agentmethod()
+    def archiveDump(self, msg, archivepath, dumpfile=None):
+        create = True
+        if exists(archivepath):
+            if not isdir(archivepath):
+                return False
+
+            create = False
+
+        if create:
+            log.info('Making archive dir: {}'.format(archivepath))
+            makedirs(archivepath)
+        
+        df = dumpfile if dumpfile else self.dumpfile
+        destname = '{}-{}-{}'.format(testbed.getNodeName(), self.name, basename(df))
+        copyfile(df, path_join(archivepath, destname))
+        log.info('Copied file: {} --> {}'.format(df, path_join(archivepath, destname)))
+        return True
+
+    def confirmConfiguration(self):
+        return True
 
 def getAgent(**kwargs):
     agent = TcpdumpAgent()
@@ -71,7 +115,21 @@ def getAgent(**kwargs):
     return agent
 
 if __name__ == "__main__":
+    from sys import argv
+
+    # run directly if debugging.
+    if '-d' in argv:
+        from time import sleep
+        logging.basicConfig(level=logging.DEBUG)
+        a = getAgent()
+        a.startCollection(None, 'host server1')
+        sleep(10)
+        a.stopCollection(None)
+        a.archiveDump(None, '/tmp/archives')
+        exit(0)
+
+    # else run as process agent.
     agent = TcpdumpAgent()
-    kwargs = initializeProcessAgent(agent, sys.argv)
+    kwargs = initializeProcessAgent(agent, argv)
     agent.setConfiguration(None, **kwargs)
     agent.run()
